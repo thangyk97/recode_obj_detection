738b770250b8af98af116621e905cf381698e36e
thang nguyen dinh <thangyk97@gmail.com>
2018-03-30 15:44:00 +0700

    editting

--- main.py
+++ main.py
@@ -412,7 +412,23 @@ for i in range(1, nb_conv + 1):
         var        = weight_reader.read_bytes(size)
 
         weights    = norm_layer.set_weights([gamma, beta, mean, var])
+
     if len(conv_layer.get_weights()) > 1:
+        bias    = weight_reader.read_bytes(
+            np.prod(conv_layer.get_weights()[1].shape))
+        kernel  = weight_reader.read_bytes(
+            np.prod(conv_layer.get_weights()[0].shape))
+        kernel  = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
+        kernel  = kernel.transpose([2,3,1,0])
+        conv_layer.set_weights([kernel])
+    else:
+        kernel = weight_reader.read_bytes(
+            np.prod(conv_layer.get_weights()[0].shape))
+        kernel = kernel.reshape(
+            list(reversed(conv_layer.get_weights()[0].shape)))
+        kernel = kernel.transpose([2,3,1,0])
+        conv_layer.set_weights([kernel])
+
 
 
 
